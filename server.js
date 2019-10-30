848b8c9670b4b58711cf3a5c1c2c9086c068837d
Adedeji Mariam <maadedeji76@student.lautech.edu.ng>
2019-10-30 13:55:36 +0000

    use logger as middleware to log reguest method, url and timestamp on every api

--- server.js
+++ server.js
@@ -4,14 +4,17 @@ const server = express();
 
 server.use('/api/posts', router)
 
-server.post('/', (req, res) => {
+server.post('/', logger, (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`)
 });
 
 //custom middleware
 
 function logger(req, res, next) {
-
+  console.log(req.method);
+  console.log(req.url);
+  console.log(Date.now())
+  next()
 };
 
 module.exports = server;
